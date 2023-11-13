fe687ecb2bbea534dfa14f2d6b38915190eec6a3
Aaron Brooks <aaron@brooks1.net>
2023-11-13 07:49:15 -0500

    flake.nix: nix-homebrew is working
    Not sure why I can't reference the imported nix-homebrew directly and need to
    add the homebrew definition to the let.

--- .config/nix-darwin/flake.nix
+++ .config/nix-darwin/flake.nix
@@ -5,16 +5,17 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
-    #nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
-    #homebrew-core   = { flake = false; url = "github:homebrew/homebrew-core"; };
-    #homebrew-cask   = { flake = false; url = "github:homebrew/homebrew-cask"; };
-    #homebrew-bundle = { flake = false; url = "github:homebrew/homebrew-bundle"; };
+    nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
+    homebrew-core   = { flake = false; url = "github:homebrew/homebrew-core"; };
+    homebrew-cask   = { flake = false; url = "github:homebrew/homebrew-cask"; };
+    homebrew-bundle = { flake = false; url = "github:homebrew/homebrew-bundle"; };
   };
 
-  #outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew, homebrew-bundle, homebrew-core, homebrew-cask, ...}:
-  outputs = inputs@{ self, nix-darwin, nixpkgs}:
+  outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew, homebrew-bundle, homebrew-core, homebrew-cask, ...}:
+  #outputs = inputs@{ self, nix-darwin, nixpkgs}:
   let
     user = "abrooks";
+    homebrew = nix-homebrew;
     configuration = { pkgs, ... }: {
       # List packages installed in system profile. To search by name, run:
       # $ nix-env -qaP | grep wget
@@ -24,6 +25,12 @@
           pkgs.neovim
           pkgs.vim
         ];
+
+      homebrew = {
+        enable = true;
+        brews = [
+        ];
+      };
 
       # Auto upgrade nix package and the daemon service.
       services.nix-daemon.enable = true;
@@ -53,21 +60,21 @@
     darwinConfigurations."Aarons-MacBook-Pro" = nix-darwin.lib.darwinSystem {
       modules = [
         configuration
-        #nix-homebrew.darwinModules.nix-homebrew
-        #{
-        #  nix-homebrew = {
-        #    enable = true;
-        #    user = "${user}";
-        #    enableRosetta = true;
-        #    taps = {
-        #      "homebrew/homebrew-core"   = homebrew-core;
-        #      "homebrew/homebrew-cask"   = homebrew-cask;
-        #      "homebrew/homebrew-bundle" = homebrew-bundle;
-        #    };
-        #    mutableTaps = false;
-        #    #autoMigrate = true;
-        #  };
-        #}
+        homebrew.darwinModules.nix-homebrew
+        {
+          nix-homebrew = {
+            enable = true;
+            user = "${user}";
+            enableRosetta = true;
+            taps = {
+              "homebrew/homebrew-core"   = homebrew-core;
+              "homebrew/homebrew-cask"   = homebrew-cask;
+              "homebrew/homebrew-bundle" = homebrew-bundle;
+            };
+            mutableTaps = false;
+            #autoMigrate = true;
+          };
+        }
       ];
     };
 
